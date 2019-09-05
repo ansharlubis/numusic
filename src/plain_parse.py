e83f1f9aefbaecfeee97bce214a14956f11ae0ae
ルスファン ルビス <luthfan.lubis@i.nau.co.jp>
2019-09-05 16:00:57 +0900

    added plain_parse

---
+++ src/plain_parse.py
@@ -0,0 +1,23 @@
+from header import *
+from block import *
+from assignment import *
+from optimization import optimize
+from time import time
+import sys
+
+def parse(path):
+	data = open(path, 'r')
+	key = meta_data(parse_header(data))[1]
+	instruments_list = parse_score(data).flatten().to_instrument(key).single_list()
+	return instruments_list
+
+def main():
+	instruments_list = parse(sys.argv[1])
+	initial_assignment = assignment_original(instruments_list, 1)
+
+	for player in initial_assignment:
+		print(player)
+		print()
+
+if __name__ == "__main__":
+    main()
